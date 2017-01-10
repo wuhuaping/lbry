07d90e063661799843c3ecca5514b9301809d284
Jack Robison <jackrobison@lbry.io>
2017-01-10 15:47:56 -0500

    try fixing travis

--- packaging/scripts/run_tests.sh
+++ packaging/scripts/run_tests.sh
@@ -1,9 +1,9 @@
 # have to do `which trial` instead of simply trial because coverage needs the full path
-pip install cython
+
+pip install Cython --install-option="--no-cython-compile"
 pip install unqlite
 pip install mock
 pip install coveralls
-
 
 coverage run --source=lbrynet `which trial` tests
 coveralls
